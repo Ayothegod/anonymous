1e634b235e7692d786ef73d434364a0f56aea6e9
Ayo <heyayomideadebisi@gmail.com>
2023-06-25 23:17:39 +0100

    solve: got supabase signup,foreign key, auth-profiles table creation and linking etc

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseUrl = "https://tjiecbhqyejybvgbusxi.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqaWVjYmhxeWVqeWJ2Z2J1c3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc3MjI2NTMsImV4cCI6MjAwMzI5ODY1M30.Lf0R2X7CGj4rdoQoUXQRWfmfEr6xJpTs05luety_Id8"
+const supabaseUrl = "https://beghxywudtulakxqbtnj.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlZ2h4eXd1ZHR1bGFreHFidG5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc3MzAxMjAsImV4cCI6MjAwMzMwNjEyMH0.k18bn8gZBs2l3c2HKgHBSHrUMVrkgppK-oqGxTRIv0E"
 
 // mtIhF6yThBRLdDZQ
 export const supabaseClient = createClient(supabaseUrl, supabaseKey)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 // import { Inter } from 'next/font/google'
 import Header from "@/components/Header";
 import { supabaseClient } from "@/lib/supabase";
+import { useEffect } from "react";
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+  useEffect(() => {
+    const getData = async() => {
+      let { data: messages, error } = await supabaseClient
+  .from('messages')
+  .select(`
+  id,
+  profiles (
+   id,full_name
+  )
+`)
+
+    console.log("message",messages);
+    
+      }
+      getData()
+  },[])
   
   async function signUpWithEmail() {
     const { data, error } = await supabaseClient.auth.signUp({
@@ -12,11 +29,13 @@ export default function Home() {
       password: 'example-password',
       options: {
         data: {
-          first_name: 'John',
-          age: 27,
+          full_name: 'John',
+          username: 'aiiomide',
         },
       },
     })
+
+    console.log(data,error);
   }
 
   async function signInWithEmail() {
@@ -25,6 +44,16 @@ export default function Home() {
       password: 'example-password',
     })
     console.log(data,error);
+  }
+
+  async function getMetadata() {
+    const {
+      data: { user },
+    }: any = await supabaseClient.auth.getUser()
+    // let metadata = user.user_metadata
+    console.log(user);
+    
+    
   }
   
   return (
@@ -35,6 +64,7 @@ export default function Home() {
         <Header/>
 
 
+        <button onClick={getMetadata} className="border border-slate-200 p-2 mx-4 rounded">Get metadata</button>
         <button onClick={signUpWithEmail} className="border border-slate-200 p-2 mx-4 rounded">Add users</button>
         <button onClick={signInWithEmail} className="border border-slate-200 p-2 mx-4 rounded">login</button>
       </main>
