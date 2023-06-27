81cc3278aa0706ea80b2a82c412495975b9d8247
Ayo <heyayomideadebisi@gmail.com>
2023-06-27 20:45:27 +0100

    add react cliptoclipboard

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -9,24 +9,24 @@ const User = () => {
   useEffect(() => {
     const getCurrentUser = async () => {
       const { data: { user } } = await supabaseClient.auth.getUser()
-
       setUsername(user?.user_metadata.username)
-      // console.log(user?.user_metadata.username);
+      // console.log(user);
     }
-    // getCurrentUser()
+    getCurrentUser()
   }, [])
-
 
   return (
     <div className="px-2 max-w-[60rem] mx-auto">
-      {/* <div>welcome {username}</div> */}
       <nav className="p-2 flex item-center justify-between border-b border-b-slate-200 relative">
         <h1 className="text-2xl font-semibold">Anonymous</h1>
         <div>
           <Dropdown />
         </div>
-
       </nav>
+
+      <div className="my-4 md:mx-20">
+        <h2 className="text-lg mx-4">welcome <span className="font-semibold text-2xl capitalize">{username}</span></h2>
+      </div>
     </div>
   )
 }
