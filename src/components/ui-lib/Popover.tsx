f4267e226beb301a26b525130523a93cc78b102d
Ayo <heyayomideadebisi@gmail.com>
2023-06-28 22:59:35 +0100

    add update username

--- src/components/ui-lib/Popover.tsx
+++ src/components/ui-lib/Popover.tsx
@@ -4,9 +4,16 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 import { useUserContext } from "@/hooks/UserContext"
+import { supabaseClient } from "@/lib/supabase"
 import { useState } from "react"
 
-const Popover = ({ username, setUsername, value, popcontent }: any) => {
+const Popover = ({ username, setUsername, value, popcontent, updateUsername }: any) => {
+    // const updateUser = async () => {
+    //     const { data, error } = await supabaseClient.auth.updateUser({
+    //         data: { username: 'brother@gmail.com' }
+    //     })
+    //     console.log(data,error);
+    // }
 
     return (
         <div>
@@ -14,8 +21,8 @@ const Popover = ({ username, setUsername, value, popcontent }: any) => {
                 <PopoverTrigger className="bg-black text-white text-sm py-2 px-6 rounded font-semibold w-[90vw] sm:w-60">{value}</PopoverTrigger>
                 <PopoverContent>
                     <div className="flex flex-col gap-2">
-                        <input type="text" placeholder={username} value={username} onChange={e => setUsername(e.target.value)} className="border border-slate-300 rounded py-1 px-2 outline-slate-600 w-full"/>
-                        <button className="bg-slate-800 text-white text-sm py-2 px-6 rounded font-semibold" >{popcontent}</button>
+                        <input type="text" placeholder={username} value={username} onChange={e => setUsername(e.target.value)} className="border border-slate-300 rounded py-1 px-2 outline-slate-600 w-full" />
+                        <button className="bg-slate-800 text-white text-sm py-2 px-6 rounded font-semibold" onClick={updateUsername}>{popcontent}</button>
                     </div>
                 </PopoverContent>
             </Pop>
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -5,14 +5,26 @@ import { useState } from "react"
 import { LogOut } from "lucide-react"
 import useSignout from "@/hooks/useSignout"
 import { useUserContext } from "@/hooks/UserContext"
+import { supabaseClient } from "@/lib/supabase"
 
 const Settings = () => {
   const signout: any = useSignout()
   const user = useUserContext()
   console.log(user);
-  const [username, setUsername] = useState<any>(user?.user_metadata.username)
+  if(user) {
+    const usernameString = user?.user_metadata?.username
+  }
+  const [username, setUsername] = useState<any>(user?.user_metadata?.username)
   const [email,setEmail] = useState(user?.email)
   const [password,setPassword] = useState("")
+
+  const updateUsername = async () => {
+    const { data, error } = await supabaseClient.auth.updateUser({
+        data: { username: 'brother@gmail.com' }
+    })
+    console.log(data,error);
+}
+
   return (
     <div className="px-2 max-w-[60rem] mx-auto">
       <nav className="p-2 flex item-center justify-between border-b border-b-slate-200 relative">
@@ -30,7 +42,7 @@ const Settings = () => {
           <div className="grid grid-cols-1 sm:grid-cols-2 w-full p-4 gap-4">
 
             <div className="flex flex-col items-center justify-center">
-              <Popover username={username} popcontent="Correct Username" value="Update Username" setUsername={setUsername} />
+              <Popover username={username} popcontent="Correct Username" value="Update Username" setUsername={setUsername} updateUsername={updateUsername} />
             </div>
             <div className="flex flex-col items-center justify-center">
               <Popover username={email} popcontent="Correct Email" value="Update Email" setUsername={setEmail} />
